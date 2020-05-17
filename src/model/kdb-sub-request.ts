b536e4ea9ac0b1a14ac768db49d04a5d52a096ac
7DG <danielrgorrod@gmail.com>
2020-05-17 15:19:32 +0100

    added subscription and sub request objects

--- src/model/kdb-sub-request.ts
+++ src/model/kdb-sub-request.ts
@@ -1,4 +1,22 @@
 import {kdbEpoch} from './kdb-request-config'
 
+export class KdbSubscriptionRequest {
+
+    time: string;
+    refId: string;
+    panelId: string;
+    subscription: KdbSubscription;
+    format: string;
+    version: string;
+}
+
+export class KdbSubscription {
+    table: string;
+    temporal_col: string;
+    select_cols: string[];
+    where_cols: string[];
+    grouping_col: string;
+}
+
 export const liveSubscriptionRequest: string =  ' {[dict] \n ' +
 ' '
