2f91c2905f3e908411fd6876062d9ce9cfbb7775
7DG <danielrgorrod@gmail.com>
2020-05-18 00:24:50 +0100

    began snapshot typing + query

--- src/model/kdb-snapshot.ts
+++ src/model/kdb-snapshot.ts
@@ -1,2 +1,15 @@
-export const liveSnapshot: string =  '{[dict] } \n ' +
-' '
+export class KdbSnapshot {
+
+    table: string;
+    temporal_col: string;
+    temporal_range: number[];
+    where_cols: string[];
+    grouping_col: string;
+    select_cols: string[];
+    maxRowCount: number;
+
+}
+
+export const liveSnapshot: string =  '{[dict] \n ' +
+' ; \n ' +
+' };'
